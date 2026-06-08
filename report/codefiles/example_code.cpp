eab55eef7a00908e93c547459c08e7e73265c71d
simago02 <simoneago44@gmail.com>
2026-06-08 12:43:43 +0200

    Add first report without modifies

---
+++ report/codefiles/example_code.cpp
@@ -0,0 +1,9 @@
+#using namespace std
+#include lib.h
+
+void main(int n1){
+    int a = 1;      // The first number
+    int b = 2;      // The second number
+    return a+b*n1;  // The sum of the numbers
+}
+    
