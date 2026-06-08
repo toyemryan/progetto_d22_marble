37300f995d7255167b87a5bd28ed60e12706ca74
simago02 <simoneago44@gmail.com>
2026-06-08 17:53:04 +0200

    Fixed appendices part

--- report/codefiles/example_code.cpp
+++
@@ -1,9 +0,0 @@
-#using namespace std
-#include lib.h
-
-void main(int n1){
-    int a = 1;      // The first number
-    int b = 2;      // The second number
-    return a+b*n1;  // The sum of the numbers
-}
-    
